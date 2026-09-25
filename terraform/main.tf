307bf4a5aff6edbc3a2a10cfcc26917cf8493e41
cbrcoding-burley <cbrcoding@gmail.com>
2026-09-24 20:24:42 -0400

    Reverting it all and removing the cloudflare resources so we can just move on easily (#2)
    This reverts commit 684de2b9342c601ac30205589e7e04f625b66e24.
    
    Revert "Mistake on the cloudflare zone filter syntax"
    
    This reverts commit ef7e7c9c071f36d40fb42cd1cace9d10e6b5330a.
    
    Revert "updated terraform versions"
    
    This reverts commit a83a7c7f6a404cdc0fd2e45ea6344d8ce055b78e.
    
    Revert "Migrate Cloudflare resources to provider v5 API"
    
    This reverts commit 5c883fd691646f7fcbda7a29bd4dbdf2c59f3531.
    
    Removing the cloudflare shit
    
    Co-authored-by: Christian Reynolds <christian.reynolds@flatiron.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -142,9 +142,7 @@ resource "railway_variable" "cron_calendar_url_staging" {
 
 data "cloudflare_zone" "this" {
   count = var.root_domain != "" ? 1 : 0
-  filter = {
-    name = var.root_domain
-  }
+  name  = var.root_domain
 }
 
 resource "railway_custom_domain" "web" {
@@ -159,44 +157,4 @@ resource "railway_custom_domain" "staging" {
   domain         = "staging.${var.root_domain}"
   service_id     = railway_service.web.id
   environment_id = local.staging_environment_id
-}
-
-resource "cloudflare_dns_record" "web" {
-  count   = var.root_domain != "" ? 1 : 0
-  zone_id = data.cloudflare_zone.this[0].id
-  name    = var.root_domain
-  content = railway_custom_domain.web[0].dns_record_value
-  type    = "CNAME"
-  proxied = true
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "web_verification" {
-  count   = var.root_domain != "" ? 1 : 0
-  zone_id = data.cloudflare_zone.this[0].id
-  name    = railway_custom_domain.web[0].verification_host_label
-  content = railway_custom_domain.web[0].verification_record_value
-  type    = "TXT"
-  proxied = false
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "staging" {
-  count   = var.enable_staging && var.root_domain != "" ? 1 : 0
-  zone_id = data.cloudflare_zone.this[0].id
-  name    = "staging.${var.root_domain}"
-  content = railway_custom_domain.staging[0].dns_record_value
-  type    = "CNAME"
-  proxied = true
-  ttl     = 1
-}
-
-resource "cloudflare_dns_record" "staging_verification" {
-  count   = var.enable_staging && var.root_domain != "" ? 1 : 0
-  zone_id = data.cloudflare_zone.this[0].id
-  name    = railway_custom_domain.staging[0].verification_host_label
-  content = railway_custom_domain.staging[0].verification_record_value
-  type    = "TXT"
-  proxied = false
-  ttl     = 1
 }
